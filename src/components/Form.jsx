db666c218d4f71e7b9996817e9020b5c74f3d406
Gerson Goatache <gersongoatache@gmail.com>
2020-05-05 11:32:12 -0400

    dynamic data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,33 @@ import TextField from '@material-ui/core/TextField';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { data } from '../formData';
+
+const renderInput = ({type, label, id}) => {
+    let input = null;
+    switch(type){
+        case "text":
+            input = <TextField
+            id={id}
+            label={label}
+            InputProps={{
+                startAdornment: (
+                <InputAdornment position="start">
+                    <AccountCircle />
+                </InputAdornment>
+                ),
+            }}
+            />;
+            break;
+        default:
+            break;
+    }
+    return input;
+}
+
+const getInput = (page) => {
+    return data[page].fields.map(field => renderInput(field));
+}
 
 const Form = () => {
   
@@ -12,28 +39,7 @@ const Form = () => {
 
     return (
         <FormControl style={{margin: "1em"}}>
-            <TextField
-            id="first-name-input"
-            label="First Name"
-            InputProps={{
-                startAdornment: (
-                <InputAdornment position="start">
-                    <AccountCircle />
-                </InputAdornment>
-                ),
-            }}
-            />
-             <TextField
-            id="last-name-input"
-            label="Last Name"
-            InputProps={{
-                startAdornment: (
-                <InputAdornment position="start">
-                    <AccountCircle />
-                </InputAdornment>
-                ),
-            }}
-            />
+            { getInput(page) }
             <div style={{display: "flex"}}>
                 <Button variant="contained" startIcon={<NavigateNextIcon style={{transform: "rotate(180deg)"}}/>}  onClick={() => changePage(page-1)} disabled={page === 0}>
                     Back
---
+++ src/formData.js
@@ -0,0 +1,16 @@
+export const data = [
+    {
+        page: 0,
+        fields: [
+            {type: "text", name: "First Name", label: "First Name", id: "first-name-input"},
+            {type: "text", name: "Last Name", label: "Last Name", id: "last-name-input"}
+        ]
+    },
+    {
+        page: 1,
+        fields: [
+            {type: "text", name: "Favorite Movie", label: "Favorite Movie", id: "movie-input"}
+        ]
+    }
+
+];
