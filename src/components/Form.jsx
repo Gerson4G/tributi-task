a0ea7794efe724309035c8b289e3735b27893e7c
Gerson Goatache <gersongoatache@gmail.com>
2020-05-05 11:16:51 -0400

    basic buttons and page state

---
+++ src/components/Form.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const Form = () => {
+  
+    const [page, changePage] = useState(0);
+
+    return (
+        <FormControl style={{margin: "1em"}}>
+            <TextField
+            id="first-name-input"
+            label="First Name"
+            InputProps={{
+                startAdornment: (
+                <InputAdornment position="start">
+                    <AccountCircle />
+                </InputAdornment>
+                ),
+            }}
+            />
+             <TextField
+            id="last-name-input"
+            label="Last Name"
+            InputProps={{
+                startAdornment: (
+                <InputAdornment position="start">
+                    <AccountCircle />
+                </InputAdornment>
+                ),
+            }}
+            />
+            <div style={{display: "flex"}}>
+                <Button variant="contained" startIcon={<NavigateNextIcon style={{transform: "rotate(180deg)"}}/>}  onClick={() => changePage(page-1)} disabled={page === 0}>
+                    Back
+                </Button>
+                <div> Page {page+1}</div>
+                <Button variant="contained" color="primary" endIcon={<NavigateNextIcon />} onClick={() => changePage(page+1)} disabled={page === 5}>
+                    Next
+                </Button>
+            </div>
+        </FormControl>
+    );
+  }
+
+  export default Form;
