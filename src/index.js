13534c51382264084802356069f5e8a3a031e5aa
Gerson Goatache <gersongoatache@gmail.com>
2020-09-30 12:20:43 -0400

    2nd dummy commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-// dummy commit
+// dummy commit 2
 ReactDOM.render(
   <React.StrictMode>
     <App />
